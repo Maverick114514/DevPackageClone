ae51f6ce80e51535b2afc3660e4c971bf0e9e24d
Aminos <147380126+2497812382@users.noreply.github.com>
2026-09-14 10:22:07 +0800

    no message

--- Assets/ModifyTest.cs
+++ Assets/ModifyTest.cs
@@ -10,14 +10,7 @@ public class ModifyTest : MonoBehaviour
         int doSth = 0;
         int doSth0 = 0;
         
-        
-        
-        
-        
-        
-        
-        
-        
+
     }
 
     // Update is called once per frame
